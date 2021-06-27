b84f68b97a91f4da0bcbbe8b101fa88a624a9725
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-06-27 22:14:37 +0700

    first commit

---
+++ src/main/java/controller/SmartphoneController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import model.Smartphone;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import service.ISmartphoneService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/smartphones")
+public class SmartphoneController {
+    @Autowired
+    private ISmartphoneService smartphoneService;
+
+    @PostMapping
+    public ResponseEntity<Smartphone> createSmartphone(@RequestBody Smartphone smartphone) {
+        return new ResponseEntity<>(smartphoneService.save(smartphone), HttpStatus.CREATED);
+    }
+    @GetMapping("/list")
+    public ModelAndView getAllSmartphonePage() {
+        ModelAndView modelAndView = new ModelAndView("/phones/list");
+        modelAndView.addObject("smartphones", smartphoneService.findAll());
+        return modelAndView;
+    }
+    @GetMapping
+    public ResponseEntity<Iterable<Smartphone>> allPhones() {
+        return new ResponseEntity<>(smartphoneService.findAll(), HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Smartphone> deleteSmartphone(@PathVariable Long id) {
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        smartphoneService.remove(id);
+        return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.NO_CONTENT);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Smartphone> editSmartphone(@PathVariable Long id,@RequestBody Smartphone smartphone){
+        System.out.println("tc");
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        smartphone.setId(smartphoneOptional.get().getId());
+        return new ResponseEntity<>(smartphoneService.save(smartphone), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Smartphone> findById(@PathVariable Long id){
+        Optional<Smartphone> smartphoneOptional = smartphoneService.findById(id);
+        if (!smartphoneOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else {
+            return new ResponseEntity<>(smartphoneOptional.get(), HttpStatus.OK);
+        }
+    }
+
+}
---
+++ src/main/java/service/ISmartphoneService.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+
+    void update(long id, Smartphone smartphone);
+}
